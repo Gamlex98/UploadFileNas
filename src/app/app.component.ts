f3db96c35c8751ea6c20c4b3b92fab150e98afd8
Alexander Obando Cordoba <alexandercor1998.ac@gmail.com>
2023-05-05 16:45:17 -0600

    Verificacion archivos existentes FUNCIONAL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from './upload-service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,9 @@ export class AuthenticationComponent {
   sid = '';
   porcentaje: number = 0;
   enviando: boolean = false;
+  overwrite= false;
+
+  // listaArchivos: DocumentModel [] = [];
 
   constructor(private authService: AuthenticationService,private http: HttpClient) {}
 
@@ -39,32 +43,71 @@ export class AuthenticationComponent {
   }
 
   onUpload() {
+    const uploadUrl =`http://172.16.1.24:8095/cgi-bin/filemanager/utilRequest.cgi?func=upload&type=standard&sid=${this.sid}&dest_path=/Intranet/prueba&overwrite=1&progress=-Intranet`;
+  
+    if (!this.fileToUpload) {
+      alert("Debes seleccionar un archivo");
+      console.log("No seleccionó archivo");
+      return;
+    }
+      // Obtener la lista de archivos
+      this.authService.getList(this.sid).subscribe(
+        async (data) => {
+          console.log(data);  
+          const existingFile = data.find((file: { filename: string; }) => file.filename.toLowerCase() === this.fileToUpload.name.toLowerCase());
+          if (existingFile) {
+            const result = await Swal.fire({
+              title: 'El archivo ya existe. ¿Qué deseas hacer?',
+              icon: 'question',
+              showCancelButton: true,
+              confirmButtonText: 'Sobrescribir',
+              cancelButtonText: 'Cambiar nombre',
+            });
+          if (result.isConfirmed) {
+              this.uploadFile(uploadUrl);
+            } else if (result.dismiss == Swal.DismissReason.cancel) {
+              // Permitir al usuario cambiar el nombre del archivo
+              const newName = prompt("Por favor ingrese un nuevo nombre para el archivo:");
+              const extensionIndex = this.fileToUpload.name.lastIndexOf('.');
+              const extension = this.fileToUpload.name.substring(extensionIndex);
+              const newFilename = `${newName}${extension}`;
+              if (newFilename) {
+                const newFile =  new File([this.fileToUpload], newFilename, { type: this.fileToUpload.type });
+                this.fileToUpload = newFile;
+                this.onUpload();
+              } else {
+                alert("Debe ingresar un nombre válido para el archivo.");
+              }
+            }
+          } else {
+            this.uploadFile(uploadUrl);
+          }
+        },
+        (error) => {
+          console.error("Error al obtener la lista de archivos", error);
+        }
+      );
+  }
 
-    const uploadUrl = `http://172.16.1.24:8095/cgi-bin/filemanager/utilRequest.cgi?func=upload&type=standard&sid=${this.sid}&dest_path=/Intranet&overwrite=1&progress=-Intranet`;
-
-    if(this.fileToUpload) {
+  uploadFile(uploadUrl: string) {
     const formData = new FormData();
     formData.append('file', this.fileToUpload, this.fileToUpload.name);
     console.log("nombre archivo : " + this.fileToUpload.name);
     console.log(" Form data : " + formData);
-
+  
     const headers = new HttpHeaders();
     headers.append('Content-Type', 'multipart/form-data');
     headers.append('Accept', 'application/json');
-
+  
     this.http.post(uploadUrl, formData, { headers }).subscribe(
       response => {
         console.log('Upload successful', response);
       },
       error => {
-        console.error('Upload failed', error);
+         console.error('Upload failed', error);
       }
     );
     this.progressBar();
-    } else {
-      alert("Debes seleccionar un archivo");
-      console.log("No selecciono archivo");
-    }
   }
 
   progressBar() {
--- src/app/upload-service.ts
+++ src/app/upload-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,7 @@ import { Observable, map } from 'rxjs';
 
 export class AuthenticationService {
 
-  baseUrl = 'http://172.16.1.24:8095/cgi-bin/authLogin.cgi?';
+  urlNas = 'http://172.16.1.24:8095/cgi-bin/authLogin.cgi?';
 
   constructor(private http: HttpClient) { }
 
@@ -16,12 +16,25 @@ export class AuthenticationService {
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
     const body = `user=${username}&pwd=${password}`;
 
-    return this.http.post(this.baseUrl, body, { headers, responseType: 'text' }).pipe(
+    return this.http.post(this.urlNas, body, { headers, responseType: 'text' }).pipe(
       map(response => {
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(response, 'text/xml');
         const authSid = xmlDoc.getElementsByTagName('authSid')[0].childNodes[0].nodeValue;
         return authSid;
+      })
+    );
+  }
+
+  getList(sid : string): Observable<any> {
+    const url = `http://172.16.1.24:8095/cgi-bin/filemanager/utilRequest.cgi?func=get_list&sid=${sid}&is_iso=0&list_mode=all&path=/Intranet/prueba&dir=ASC&limit=1000&sort=filename&start=0`;
+    return this.http.get(url).pipe(
+      map((response: any) => {
+        return response.datas;
+      }),
+      catchError(error => {
+        console.error('Error al obtener la lista de archivos', error);
+        return throwError(error);
       })
     );
   }
